f463bf613c0f2b04ee0dbfb41af30709afa35bc8
Konstantin Tretyakov <kt@ut.ee>
2014-04-01 00:14:06 +0300

    Initial version

---
+++ src/rtd_test/tests/basic_test.py
@@ -0,0 +1,13 @@
+'''
+rtd-test: Test module.
+
+Meant for use with py.test.
+Write each test as a function named test_<something>.
+Read more here: http://pytest.org/
+
+Copyright 2014, 
+Licensed under MIT
+'''
+
+def test_example():
+    assert True
